c9ddadfcf1e55cd336f6c45d6952e42be341a10b
thomas <thomasjunginbox@gmail.com>
2015-11-30 13:29:36 -0800

    Write tests for user create form and post action

---
+++ spec/request/users_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "GET /users/new", type: :request do
+  it "gets the user create page" do
+    get "/users/new"
+    expect(response.status).to eq(200)
+    expect(response.body).to include("<h1>Create User</h1>")
+  end
+end
+
+describe 'POST /users', type: :request do
+  it "creates a user" do
+    VCR.use_cassette('user cassette') do
+      post "/users", { 'email': 'johndoe@email.com', 'password': 'secret' }
+      expect(response.status).to eq(200)
+      expect(response.body).to include("<h1>Create User</h1>")
+    end
+  end
+end
